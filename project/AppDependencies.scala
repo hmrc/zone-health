29fdd5a595b4d963f91a6e6132900714649533e8
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2021-07-28 11:59:43 +0100

    TEL-2569: added val for bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,25 +4,26 @@ import sbt._
 
 object AppDependencies {
 
+  val hmrcBootstrapVersion = "5.7.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.7.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "simple-reactivemongo"      % "8.0.0-play-28",
     "com.iheart"        %% "ficus"                     % "1.4.5",
     PlayImport.ws
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"             % Test,
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"           % Test,
-//    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28"     % Test,
-    "org.mockito"            %% "mockito-scala-scalatest"  % "1.16.23"           % Test
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % hmrcBootstrapVersion % Test,
+    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"            % Test,
+    "org.mockito"            %% "mockito-scala-scalatest"  % "1.16.23"            % Test
   )
 
   val itTest = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"             % "it",
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"           % "it",
-    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28"     % "it",
-    "org.mockito"            %% "mockito-scala-scalatest"  % "1.16.23"           % "it",
-    "com.dimafeng"           %% "testcontainers-scala"     % "0.39.5"            % "it"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % hmrcBootstrapVersion % "it",
+    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"            % "it",
+    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28"      % "it",
+    "org.mockito"            %% "mockito-scala-scalatest"  % "1.16.23"            % "it",
+    "com.dimafeng"           %% "testcontainers-scala"     % "0.39.5"             % "it"
   )
 }
