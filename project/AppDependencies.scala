16c788131dd17fea950decd0b468b955e662697e
jonnydh <60072280+jonnydh@users.noreply.github.com>
2024-02-16 12:38:12 +0000

    TEL-4313 Bump deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,8 @@ object AppDependencies {
 
   val hmrcBootstrapVersion = "8.4.0"
   val hmrcMongoVersion     = "1.7.0"
-  val mockitoVersion       = "1.17.29"
+  val mockitoVersion       = "1.17.30"
+  val flexmarkVersion      = "0.64.8"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % hmrcBootstrapVersion,
@@ -16,14 +17,14 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"   % hmrcBootstrapVersion % Test,
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"            % Test,
+    "com.vladsch.flexmark"   %  "flexmark-all"             % flexmarkVersion      % Test,
     "org.mockito"            %% "mockito-scala-scalatest"  % mockitoVersion       % Test
   )
 
   val itTest = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"   % hmrcBootstrapVersion % "it",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion     % "it",
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"            % "it",
+    "com.vladsch.flexmark"   %  "flexmark-all"             % flexmarkVersion      % "it",
     "org.mockito"            %% "mockito-scala-scalatest"  % mockitoVersion       % "it",
     "com.dimafeng"           %% "testcontainers-scala"     % "0.39.5"             % "it"
   )
